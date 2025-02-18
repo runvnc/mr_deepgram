d432586423219a992562120ee05e20b5aef3a41d
Jason Livesay <runvnc@gmail.com>
2025-02-18 10:16:06 -0600

    update

--- src/mr_deepgram/static/chat-stt.js
+++ src/mr_deepgram/static/chat-stt.js
@@ -260,7 +260,7 @@ class ChatSTT extends BaseEl {
             interim_results: true,
             punctuate: true,
             numerals: true,
-            keyterm: "Biolimitless, Bio limitless",
+            keyterm: ["Biolimitless", "Bio limitless"],
             endpointing: 10
           })
           console.log({socket: this.socket})

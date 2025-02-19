9a658fe1a85d04a1af74b26252c882dcab281c9b
Jason Livesay <runvnc@gmail.com>
2025-02-18 21:44:53 -0600

    update

--- src/mr_deepgram/static/chat-stt.js
+++ src/mr_deepgram/static/chat-stt.js
@@ -198,7 +198,7 @@ class ChatSTT extends BaseEl {
       if (this.partialTranscript != '' || this.transcript !== '') {
         console.log("Sending text to chat form")
         let inputText = this.transcript
-        if (inputText == "") inputText = this.partialTranscript;
+        inputText += this.partialTranscript;
         this.transcript = ""
         this.partialTranscript = ""
         
@@ -275,12 +275,12 @@ class ChatSTT extends BaseEl {
 
             if (transcript_data !== "") {
               console.log("Transcript:", transcript_data)
-              this.textInput.value = transcript_data
+              //this.textInput.value = transcript_data
               this.partialTranscript = transcript_data
 
               if (data.is_final) {
                 this.transcript += transcript_data + " "
-                
+                this.partialTranscript = "" 
                 if (!this.dontInterrupt) {
                   this.chatForm._send()
                   this.transcript = ""
